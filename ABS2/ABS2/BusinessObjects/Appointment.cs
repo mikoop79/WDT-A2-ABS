3c8cce457f375b8c27ed3fbf27f5ab83adddade3
mikoop79 <mikoop@mac.com>
2012-05-20 20:00:22 +1000

    Appointment booking attempt

---
+++ ABS2/ABS2/BusinessObjects/Appointment.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ABS2.BusinessObjects
+{
+    public class Appointment
+    {
+        String strCnn;
+        String Name;
+        SqlConnection m_DBConnection;
+        SqlCommand m_Command;
+        SqlTransaction m_Transaction;
+        DataSet m_DataSet;
+        String m_CommandText;
+        int m_Timeout = 6000;
+        Boolean m_bIsConnected;
+        Boolean m_bInTxn;
+        SqlDataAdapter c_DataAdapter;
+        DataSet dsExecSelect;
+        String ErrorMsg;
+        int ErrorNo;
+
+
+        private DataSet _ds;
+        private DataTable dt;
+        private int iRet;
+
+        //This method is to check whether the record exists in the database or not. It needs to be called before inserting the appointment.
+        //The stored procedure returns a column "Return_Value" with a value of -1(If a record already exists) OR 1(If it doesn't exists).
+        //Hence if that return_value is 1 then we can proceed with executing the method InsertAppointment
+        public DataSet CheckForDuplicateAppointment(String UserName, DateTime StartDate, DateTime EndDate, int BookingObjectID)
+        {
+            m_DBConnection = new SqlConnection();
+            strCnn = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
+            m_DBConnection.ConnectionString = strCnn;
+            m_DBConnection.Open();
+
+            m_CommandText = "usp_check_for_duplicate_Appointment";
+            m_Command = new SqlCommand(m_CommandText, m_DBConnection);
+            m_Command.CommandTimeout = m_Timeout;
+            m_Command.CommandType = CommandType.StoredProcedure;
+            dsExecSelect = new DataSet();
+            SqlParameter _UserName = new SqlParameter();
+            _UserName.ParameterName = "@UserName";
+            _UserName.SqlDbType = SqlDbType.NVarChar;
+            _UserName.Size = 256;
+            _UserName.Direction = ParameterDirection.Input;
+            _UserName.Value = UserName;
+            SqlParameter _StartDate = new SqlParameter();
+            _StartDate.ParameterName = "@StartDate";
+            _StartDate.SqlDbType = SqlDbType.DateTime;
+            _StartDate.Size = 30;
+            _StartDate.Direction = ParameterDirection.Input;
+            _StartDate.Value = StartDate;
+            SqlParameter _EndDate = new SqlParameter();
+            _EndDate.ParameterName = "@EndDate";
+            _EndDate.SqlDbType = SqlDbType.DateTime;
+            _EndDate.Size = 30;
+            _EndDate.Direction = ParameterDirection.Input;
+            _EndDate.Value = EndDate;
+            SqlParameter _BookingObjectID = new SqlParameter();
+            _BookingObjectID.ParameterName = "@BookingObjectId";
+            _BookingObjectID.SqlDbType = SqlDbType.Int;
+            _BookingObjectID.Size = 10;
+            _BookingObjectID.Direction = ParameterDirection.Input;
+            _BookingObjectID.Value = BookingObjectID;
+            m_Command.Parameters.Add(_UserName);
+            m_Command.Parameters.Add(_StartDate);
+            m_Command.Parameters.Add(_EndDate);
+            m_Command.Parameters.Add(_BookingObjectID);
+            c_DataAdapter = new SqlDataAdapter(m_Command);
+            c_DataAdapter.Fill(dsExecSelect);
+            m_DBConnection.Close();
+            m_DBConnection = null;
+            m_Command = null;
+            m_bIsConnected = false;
+
+            return dsExecSelect;
+        }
+
+        public int InsertAppointment(String UserName, String UserEmailAddress, DateTime StartDate, DateTime EndDate, String Comments, int BookingObjectID)
+        {
+            m_DBConnection = new SqlConnection();
+            strCnn = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
+            m_DBConnection.ConnectionString = strCnn;
+            m_DBConnection.Open();
+
+            m_CommandText = "usp_ins_Appointment";
+            m_Command = new SqlCommand(m_CommandText, m_DBConnection);
+            m_Command.CommandTimeout = m_Timeout;
+            m_Command.CommandType = CommandType.StoredProcedure;
+            //dsExecSelect = new DataSet();
+            SqlParameter _UserName = new SqlParameter();
+            _UserName.ParameterName = "@UserName";
+            _UserName.SqlDbType = SqlDbType.NVarChar;
+            _UserName.Size = 256;
+            _UserName.Direction = ParameterDirection.Input;
+            _UserName.Value = UserName;
+            SqlParameter _UserEmailAddress = new SqlParameter();
+            _UserEmailAddress.ParameterName = "@UserEmailAddress";
+            _UserEmailAddress.SqlDbType = SqlDbType.NVarChar;
+            _UserEmailAddress.Size = 256;
+            _UserEmailAddress.Direction = ParameterDirection.Input;
+            _UserEmailAddress.Value = UserEmailAddress;
+            SqlParameter _StartDate = new SqlParameter();
+            _StartDate.ParameterName = "@StartDate";
+            _StartDate.SqlDbType = SqlDbType.DateTime;
+            _StartDate.Size = 30;
+            _StartDate.Direction = ParameterDirection.Input;
+            _StartDate.Value = StartDate;
+            SqlParameter _EndDate = new SqlParameter();
+            _EndDate.ParameterName = "@EndDate";
+            _EndDate.SqlDbType = SqlDbType.DateTime;
+            _EndDate.Size = 30;
+            _EndDate.Direction = ParameterDirection.Input;
+            _EndDate.Value = EndDate;
+            SqlParameter _Comments = new SqlParameter();
+            _Comments.ParameterName = "@Comments";
+            _Comments.SqlDbType = SqlDbType.NVarChar;
+            _Comments.Size = 500;
+            _Comments.Direction = ParameterDirection.Input;
+            _Comments.Value = Comments;
+            SqlParameter _BookingObjectID = new SqlParameter();
+            _BookingObjectID.ParameterName = "@BookingObjectId";
+            _BookingObjectID.SqlDbType = SqlDbType.Int;
+            _BookingObjectID.Size = 10;
+            _BookingObjectID.Direction = ParameterDirection.Input;
+            _BookingObjectID.Value = BookingObjectID;
+            m_Command.Parameters.Add(_UserName);
+            m_Command.Parameters.Add(_UserEmailAddress);
+            m_Command.Parameters.Add(_StartDate);
+            m_Command.Parameters.Add(_EndDate);
+            m_Command.Parameters.Add(_Comments);
+            m_Command.Parameters.Add(_BookingObjectID);
+            iRet = m_Command.ExecuteNonQuery();
+            m_DBConnection.Close();
+            m_DBConnection = null;
+            m_Command = null;
+            m_bIsConnected = false;
+
+            return iRet;
+        }
+    }
+}
--- ABS2/ABS2/MakeAppointment.aspx.cs
+++ ABS2/ABS2/MakeAppointment.aspx.cs
@@ -5,37 +5,64 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.Security;
+using ABS2.BusinessObjects;
+using ABS2.BusinessObjects;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace ABS2
 {
     public partial class MakeAppointment : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            if (Request.QueryString.Get("roomid") != null)
-            {
-                RoomID.SelectedIndex = Convert.ToInt32(Request.QueryString.Get("roomid"));
-                MakeAppointmentWizard.ActiveStepIndex = 1;
-            }
-            if (Request.QueryString.Get("date") != null)
-            {
-                //TODO set select date
-                MakeAppointmentWizard.ActiveStepIndex = 2;
-            }
-            if (Request.QueryString.Get("time") != null)
-            {
-                //TODO set select time
-                MakeAppointmentWizard.ActiveStepIndex = 3;
-            }
-            
-            if (MakeAppointmentWizard.ActiveStepIndex == 4)
-            {
-                SummaryRoom.Text = RoomID.SelectedItem.Text;
-                SummaryDate.Text = Date.SelectedDate.ToShortDateString();
-                SummaryTime.Text = Time.SelectedItem.Text;
-                SummaryComment.Text = Comment.Text;
 
-                
+        String Email;
+        String Username;
+        DateTime StartDate;
+        DateTime EndDate;
+        String Comments;
+        int BookingObjectID;
+
+
+
+        protected void Page_Load(object sender, EventArgs e)
+
+        {
+
+            if (HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                if (Request.QueryString.Get("roomid") != null)
+                {
+                    RoomID.SelectedIndex = Convert.ToInt32(Request.QueryString.Get("roomid"));
+                    MakeAppointmentWizard.ActiveStepIndex = 1;
+                }
+                if (Request.QueryString.Get("date") != null)
+                {
+                    //TODO set select date
+                    MakeAppointmentWizard.ActiveStepIndex = 2;
+                }
+                if (Request.QueryString.Get("time") != null)
+                {
+                    //TODO set select time
+                    MakeAppointmentWizard.ActiveStepIndex = 3;
+                }
+
+                if (MakeAppointmentWizard.ActiveStepIndex == 4)
+                {
+                    SummaryRoom.Text = RoomID.SelectedItem.Text;
+                    SummaryDate.Text = Date.SelectedDate.ToShortDateString();
+                    SummaryTime.Text = Time.SelectedItem.Text;
+                    SummaryComment.Text = Comment.Text;
+                    
+                    DateTime StartTime = Convert.ToDateTime(Time.SelectedItem.Text);
+                    DateTime EndTime = Convert.ToDateTime(Date.SelectedDate.ToShortDateString());
+                    String Comments = Comment.Text;
+                    int BookingObjectID  = Convert.ToInt32(Request.QueryString.Get("roomid"));
+
+                }
+            }
+            else
+            {
+                Response.Redirect("~/Login.aspx?message=You need to be logged in to make a booking.");
             }
         }
 
@@ -61,9 +88,19 @@ namespace ABS2
 
             /*
             MembershipUser mu = Membership.GetUser();
+<<<<<<< HEAD
             string email = mu.Email;
             string username = mu.UserName;
              */
+=======
+            Email = mu.Email;
+            Username = mu.UserName;
+            Appointment ap = new Appointment();
+           
+            ap.InsertAppointment(Username, Email, StartDate, EndDate, Comments, BookingObjectID);
+
+
+>>>>>>> Appointment booking attempt
 
         }
     }
